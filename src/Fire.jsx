e3252035f6899d596746eae2752a1ecb655cd2d3
mohideentgm <87963140+mohideentgm@users.noreply.github.com>
2021-07-27 06:52:00 +0000

    REQUIRE

--- src/Fire.jsx
+++ src/Fire.jsx
@@ -2,6 +2,8 @@ import Firebase from 'firebase/app';
 import 'firebase/auth'
 import 'firebase/firestore'
 
+require('dotenv').config();
+
 const firebaseConfig = {
   apiKey: import.meta.env.API_KEY,
   authDomain: import.meta.env.AUTHDOMAINE,
@@ -18,17 +20,3 @@ const auth = Firebase.auth();
 
 export { Db, auth };
 
-
-
-
-
-
-
-
-  // apiKey: import.meta.env.VITE_API_KEY,
-  // authDomain: import.meta.env.VITE_AUTHDOMAINE,
-  // projectId: import.meta.env.VITE_PROJECTID,
-  // storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  // messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  // appId: import.meta.env.VITE_APPID,
-  // measurementId: import.meta.env.VITE_MEASUREMENTID
